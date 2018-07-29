abbc8d5d1465ee660779cbb0ebb823829b867f44
Petr Bouda <pbouda@ebay.com>
2018-07-29 13:50:53 +0200

    Kick-off

---
+++ src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module bytecode.inspection {
+}
---
+++ src/main/java/pbouda/bytecode/LambdaInspection.java
@@ -0,0 +1,9 @@
+package pbouda.bytecode;
+
+public class LambdaInspection {
+
+    public static void main(String[] args) {
+        System.out.println("Lambda Inspection");
+    }
+
+}
